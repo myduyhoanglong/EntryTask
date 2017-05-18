0cf7989fb30aa0325381432bc51955b27af60388
root <root@localhost.localdomain>
2017-05-18 12:57:36 +0800

    config database to mySQL

--- EntryTask/settings.py
+++ EntryTask/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'EntryTask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'WebDatabase',
+		'USER': 'root',
+		'PASSWORD': 'centos7',
+		'HOST': 'localhost',
     }
 }
 
