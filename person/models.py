766d64c160cdc651f2aff2841a7f0bda641c884a
My Duy Hoang Long <myduyhoanglong@gmail.com>
2017-05-20 01:24:16 +0700

    add hashing for password

--- person/models.py
+++ person/models.py
@@ -6,9 +6,10 @@ from django.db import models
 # Create your models here.
 
 class Person(models.Model):
-	username = models.CharField(max_length=150)
-	password = models.CharField(max_length=100)
-	email = models.EmailField()
+    username = models.CharField(max_length=150)
+    password = models.CharField(max_length=300)
+    salt = models.CharField(max_length=300)
+    email = models.EmailField()
 
-	def __str__(self):
-		return self.username
+    def __str__(self):
+        return '[' + self.username + '|' + self.email + ']'
